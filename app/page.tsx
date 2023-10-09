020ab6a21527d38d1cc5495001610ea71459fd58
sanaeas <sanaassim1@gmail.com>
2023-10-09 14:38:36 +0100

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import CityPicker from "@/components/CityPicker";
+import { Card, Text, Divider, Subtitle } from "@tremor/react";
+
 export default function Home() {
     return (
-        <main className="text-6xl">
-            <h1>Build a GPT Weather App</h1>
+        <main className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+            <Card className="max-w-4xl mx-auto">
+                <Text className="text-6xl font-bold text-center mb-10">
+                    Weather AI
+                </Text>
+                <Subtitle className="text-xl text-center">
+                    Powered by OpenAI, Next.js 13.5, Tailwind CSS, Tremor 2.0,
+                    and More!
+                </Subtitle>
+
+                <Divider className="my-10" />
+
+                <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+                    <CityPicker />
+                </Card>
+            </Card>
         </main>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,48 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        tremor: {
+          brand: {
+            faint: "#eff6ff", // blue-50
+            muted: "#bfdbfe", // blue-200
+            subtle: "#60a5fa", // blue-400
+            DEFAULT: "#3b82f6", // blue-500
+            emphasis: "#1d4ed8", // blue-700
+            inverted: "#ffffff", // white
+          },
+          background: {
+            muted: "#f9fafb", // gray-50
+            subtle: "#f3f4f6", // gray-100
+            DEFAULT: "#ffffff", // white
+            emphasis: "#374151", // gray-700
+          },
+          border: {
+            DEFAULT: "#e5e7eb", // gray-200
+          },
+          ring: {
+            DEFAULT: "#e5e7eb", // gray-200
+          },
+          content: {
+            subtle: "#9ca3af", // gray-400
+            DEFAULT: "#6b7280", // gray-500
+            emphasis: "#374151", // gray-700
+            strong: "#111827", // gray-900
+            inverted: "#ffffff", // white
+          },
+        },
+      },
+      boxShadow: {
+        // light
+        "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+        "tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        "tremor-dropdown": "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+      },
+      borderRadius: {
+        "tremor-small": "0.375rem",
+        "tremor-default": "0.5rem",
+        "tremor-full": "9999px",
+      },
     },
   },
   plugins: [],
