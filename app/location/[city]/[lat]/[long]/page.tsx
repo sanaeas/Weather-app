245c5034bf996be41e748e60f1a3effa52561fde
sanaeas <sanaassim1@gmail.com>
2024-02-08 15:14:03 +0100

    Fix errors

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -44,7 +44,7 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
             <div className="flex-1 p-5 lg:p-10">
                 <div className="p-5">
                     <div className="pb-5">
-                        <h2 className="text-xl font-bold">Today's Overview</h2>
+                        <h2 className="text-xl font-bold">Today&apos;s Overview</h2>
                         <p className="text-sm text-gray-400">
                             Last Updated at:{" "}
                             {new Date(
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <main className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
             <Card className="max-w-4xl mx-auto">
                 <Text className="text-6xl font-bold text-center mb-10">
-                    Weather AI
+                    Weather App
                 </Text>
                 <Subtitle className="text-xl text-center">
                     Powered by Next.js 13.5, Tailwind CSS, Tremor 2.0, and More!
